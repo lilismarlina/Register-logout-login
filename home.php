be5be7bf7adf334326408437c91fefaba07a7a2a
Lilis Marlina <114377093+lilismarlina@users.noreply.github.com>
2022-10-30 04:40:20 -0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+if (isset($_SESSION["username"])) {
+    $username = $_SESSION["username"];
+    session_write_close();
+} else {
+    // since the username is not set in session, the user is not-logged-in
+    // he is trying to access this page unauthorized
+    // so let's clear all session variables and redirect him to index
+    session_unset();
+    session_write_close();
+    $url = "./index.php";
+    header("Location: $url");
+}
+
+?>
+<HTML>
+<HEAD>
+<TITLE>Welcome</TITLE>
+<link href="assets/css/phppot-style.css" type="text/css"
+	rel="stylesheet" />
+<link href="assets/css/user-registration.css" type="text/css"
+	rel="stylesheet" />
+</HEAD>
+<BODY>
+
+<?php
+include 'functions.php';
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Welcome to the home page!</p>
+</div>
+
+<?=template_footer()?>
+    </div>
+    
+</BODY>
+</HTML>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__ . "/login.php";
+
+include 'functions.php';
+?>
